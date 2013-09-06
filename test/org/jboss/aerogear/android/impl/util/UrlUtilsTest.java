ac91e89d2fae65663dde9a06739f60f929c1ff6c
Daniel Passos <daniel@passos.me>
2013-09-06 15:41:41 -0300

    Add some UrlUtils tests

--- test/org/jboss/aerogear/android/impl/util/UrlUtilsTest.java
+++ test/org/jboss/aerogear/android/impl/util/UrlUtilsTest.java
@@ -16,17 +16,19 @@
  */
 package org.jboss.aerogear.android.impl.util;
 
+import com.google.android.gms.internal.ah;
 import org.junit.Test;
 
 import java.net.URL;
 
+import static org.jboss.aerogear.android.impl.util.UrlUtils.appendQueryToBaseURL;
 import static org.jboss.aerogear.android.impl.util.UrlUtils.appendToBaseURL;
 import static org.junit.Assert.assertEquals;
 
 public class UrlUtilsTest {
 
     @Test
-    public void testBothHaveSlash() throws Exception {
+    public void testAppendToBaseURLWithBothHaveSlash() throws Exception {
         URL baseURL = new URL("http://fakeurl.com/");
         String endpoint = "/endpoint";
         URL expectURL = new URL("http://fakeurl.com/endpoint");
@@ -35,7 +37,7 @@ public class UrlUtilsTest {
     }
 
     @Test
-    public void testOnlyBaseURLHasSlash() throws Exception {
+    public void testAppendToBaseURLWithOnlyBaseURLHasSlash() throws Exception {
         URL baseURL = new URL("http://fakeurl.com/");
         String endpoint = "endpoint";
         URL expectURL = new URL("http://fakeurl.com/endpoint");
@@ -44,7 +46,7 @@ public class UrlUtilsTest {
     }
 
     @Test
-    public void testOnlyEndpointHasSlash() throws Exception {
+    public void testAppendToBaseURLWithOnlyEndpointHasSlash() throws Exception {
         URL baseURL = new URL("http://fakeurl.com");
         String endpoint = "/endpoint";
         URL expectURL = new URL("http://fakeurl.com/endpoint");
@@ -53,12 +55,59 @@ public class UrlUtilsTest {
     }
 
     @Test
-    public void testBothWithoutSlash() throws Exception {
+    public void testAppendToBaseURLWithBothWithoutSlash() throws Exception {
         URL baseURL = new URL("http://fakeurl.com");
         String endpoint = "endpoint";
         URL expectURL = new URL("http://fakeurl.com/endpoint");
 
         assertEquals(expectURL, appendToBaseURL(baseURL, endpoint));
     }
+
+    @Test
+    public void testAppendQueryWithEmptyQuery() throws Exception {
+        URL baseURL = new URL("http://fakeurl.com/endpoint");
+        String query = "";
+        URL expectURL = new URL("http://fakeurl.com/endpoint?");
+
+        assertEquals(expectURL, appendQueryToBaseURL(baseURL, query));
+    }
+
+    @Test
+    public void testAppendQueryWithUrlEndSlash() throws Exception {
+        URL baseURL = new URL("http://fakeurl.com/endpoint/");
+        String query = "";
+        URL expectURL = new URL("http://fakeurl.com/endpoint?");
+
+        assertEquals(expectURL, appendQueryToBaseURL(baseURL, query));
+    }
+
+    @Test
+    public void testAppendQueryWithQueryStartWithQuestionMark() throws Exception {
+        URL baseURL = new URL("http://fakeurl.com/endpoint");
+        String query = "?y=2";
+        URL expectURL = new URL("http://fakeurl.com/endpoint?y%3D2");
+
+        assertEquals(expectURL, appendQueryToBaseURL(baseURL, query));
+    }
+
+    @Test
+    public void testAppendQueryWithParameter() throws Exception {
+        URL baseURL = new URL("http://fakeurl.com/endpoint?x=1");
+        String query = "y=2";
+        URL expectURL = new URL("http://fakeurl.com/endpoint?x=1?y%3D2");
+
+        assertEquals(expectURL, appendQueryToBaseURL(baseURL, query));
+    }
+
+    @Test
+    public void testAppendQueryWithBaseURLEndWithQuestionMarkAndQueryStartWithQuestionMark() throws Exception {
+        URL baseURL = new URL("http://fakeurl.com/endpoint?x=1");
+        String query = "?y=2";
+        URL expectURL = new URL("http://fakeurl.com/endpoint?x=1?y%3D2");
+
+        assertEquals(expectURL, appendQueryToBaseURL(baseURL, query));
+    }
+
+
 
 }
